b128831ff8c246b70b42e847a9660461b2333ac4
gmzelias <gmzelias@gmail.com>
2018-10-16 11:46:55 -0400

    One Tx with 2 outputs, old skeleton with 2 tx (BIG and SMALL) in commented code

--- public/js/exchange.js
+++ public/js/exchange.js
@@ -1,6 +1,6 @@
 $('#BsSAmount').on('input',function() { 
     var rate = $('#ExRa').text();
-    var exchange = $('#BsSAmount').val() / rate;
+    var exchange = ($('#BsSAmount').val() / rate)+0.00000500; // 500 Duff added as a Flat Fee
     var e = exchange.toFixed(8);
     $('#DashAmount').val(e);
 });
