18b158301a39be87bad114ee0292940f41f01274
gmzelias <gmzelias@gmail.com>
2018-11-16 11:55:08 -0400

    3rd attempt

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,16 +1,16 @@
 module.exports = {
-  apps: [{
-    name: 'PaymentProcessor',
-    script: './app.js'
+  apps : [{
+    name   : "app1",
+    script : "./app.js"
   }],
   deploy: {
     production: {
       user: 'ubuntu',
       host: 'ec2-52-24-139-164.us-west-2.compute.amazonaws.com',
-      key: '~/.ssh/PaymentProcessor.pem',
+      key: 'C:/Users/gmzel/Desktop/PaymentProcessor.pem',
       ref: 'origin/master',
-      repo: 'github.com/gmzelias/DashPaymentV1.git',
-      path: '/home/ubuntu/PaymentProcessor',
+      repo: 'https://github.com/gmzelias/DashPaymentV1.git',
+      path: '/home/ubuntu/Code/DashPayment',
       'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
     }
   }
