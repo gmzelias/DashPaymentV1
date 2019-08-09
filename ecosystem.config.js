7c4363715f9b0ed28fe501478c80b2cc7d88ea1b
gmzelias <gmzelias@gmail.com>
2019-08-09 16:20:37 -0400

    PM2 URL uptated

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,7 +6,7 @@ module.exports = {
   deploy: {
     production: {
       user: 'ubuntu',
-      host: 'ec2-52-24-139-164.us-west-2.compute.amazonaws.com',
+      host: 'ec2-18-237-86-164.us-west-2.compute.amazonaws.com',
       key: 'C:/Users/gmzel/Desktop/PaymentProcessor.pem',
       ref: 'origin/master',
       repo: 'https://github.com/gmzelias/DashPaymentV1.git',
