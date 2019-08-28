bf7c9291fa6b375644d29bcb067624fd13700586
gmzelias <gmzelias@gmail.com>
2019-08-28 16:36:25 -0400

    CORS for Wayu

--- app.js
+++ app.js
@@ -18,7 +18,8 @@ app.use(routes);
 
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method,Authorization");
+  res.header("Access-Control-Allow-Headers","*");
+  //res.header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method,Authorization");
   res.header("Access-Control-Allow-Methods", 'PUT, POST, GET, DELETE, OPTIONS');
   res.header("Allow: GET, POST, OPTIONS, PUT, DELETE");
   console.info(req.method + ' ' + req.originalUrl);
