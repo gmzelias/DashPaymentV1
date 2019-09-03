6871f8801698ff0f79970d1425ac7a8aaffdf4fe
gmzelias <gmzelias@gmail.com>
2019-09-03 19:07:00 +0200

    Cors Policy

--- app.js
+++ app.js
@@ -17,13 +17,22 @@ app.use(express.static('public'))
 app.use(routes);
 
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
+ /* res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers","*");
   //res.header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method,Authorization");
   res.header("Access-Control-Allow-Methods", 'PUT, POST, GET, DELETE, OPTIONS');
   res.header("Allow: GET, POST, OPTIONS, PUT, DELETE");
   console.info(req.method + ' ' + req.originalUrl);
-  next();
+  next();*/
+
+
+  res.setHeader('Access-Control-Allow-Origin', '*');
+
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  res.setHeader('Access-Control-Allow-Headers', '*');
+
+  res.setHeader('Access-Control-Allow-Credentials', true);
 });
 
 const port = process.env.PORT;
